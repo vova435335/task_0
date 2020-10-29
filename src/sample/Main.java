5a3e3a99b9306c7bae1fa9dc9ec9c9e5767927c5
Vladimir <vova435335>
2020-10-29 16:07:17 +0300

    Создание пользовательского интерыейса

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Процентный калькулятор");
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 
--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -1,4 +1,50 @@
 package sample.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField amount_tf;
+
+    @FXML
+    private Button calculate_small_tip_btn;
+
+    @FXML
+    private Label result_small_tip_label;
+
+    @FXML
+    private Button calculate_usual_tip_btn;
+
+    @FXML
+    private Label result_usual_tip_label;
+
+    @FXML
+    private Button calculate_big_tip_btn;
+
+    @FXML
+    private Label result_big_tip_label;
+
+    @FXML
+    void initialize() {
+        assert amount_tf != null : "fx:id=\"amount_tf\" was not injected: check your FXML file 'sample.fxml'.";
+        assert calculate_small_tip_btn != null : "fx:id=\"calculate_small_tip_btn\" was not injected: check your FXML file 'sample.fxml'.";
+        assert result_small_tip_label != null : "fx:id=\"result_small_tip_label\" was not injected: check your FXML file 'sample.fxml'.";
+        assert calculate_usual_tip_btn != null : "fx:id=\"calculate_usual_tip_btn\" was not injected: check your FXML file 'sample.fxml'.";
+        assert result_usual_tip_label != null : "fx:id=\"result_usual_tip_label\" was not injected: check your FXML file 'sample.fxml'.";
+        assert calculate_big_tip_btn != null : "fx:id=\"calculate_big_tip_btn\" was not injected: check your FXML file 'sample.fxml'.";
+        assert result_big_tip_label != null : "fx:id=\"result_big_tip_label\" was not injected: check your FXML file 'sample.fxml'.";
+
+    }
 }
