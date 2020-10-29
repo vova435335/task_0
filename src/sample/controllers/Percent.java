bbab0606ad87dfacdfe24868a7fe689bcf385530
Vladimir <vova435335>
2020-10-29 15:40:53 +0300

    Создание класса Percent

--- src/sample/controllers/Percent.java
+++ src/sample/controllers/Percent.java
@@ -1,4 +1,16 @@
 package sample.controllers;
 
 public class Percent {
+
+    double percentageOfAmount(double sum, int percent) {
+        return sum * percent / 100;
+    }
+
+    double totalAmount(double sum, int percent) {
+        return sum - percentageOfAmount(sum, percent);
+    }
+
+    int roundedTotalAmount(double sum, int percent) {
+        return ((int)Math.round(totalAmount(sum, percent)));
+    }
 }
